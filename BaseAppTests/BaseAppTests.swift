a634676f5020fdcf5672ddde292f107eca3c6de0
Dongcp <d-chu.run@gmofh.com>
2019-08-02 15:50:40 +0900

    Merge branch 'develop' into test
    # Conflicts:
    #	BaseApp.xcodeproj/project.xcworkspace/xcuserdata/chuphuongdong.xcuserdatad/UserInterfaceState.xcuserstate

--- BaseAppTests/BaseAppTests.swift
+++ BaseAppTests/BaseAppTests.swift
@@ -22,7 +22,7 @@ class BaseAppTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual("a", "a")
+        XCTAssertEqual("a", "c")
     }
     
     func testExample2222() {

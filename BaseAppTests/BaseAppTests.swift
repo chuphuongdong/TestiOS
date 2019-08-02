22451db18c300e6316244eea8c119f9f908d4755
Dongcp <d-chu.run@gmofh.com>
2019-08-02 15:47:52 +0900

    push 6

--- BaseAppTests/BaseAppTests.swift
+++ BaseAppTests/BaseAppTests.swift
@@ -24,6 +24,12 @@ class BaseAppTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual("a", "a")
     }
+    
+    func testExample2222() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual("a", "b")
+    }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.

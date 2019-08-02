27bb3a0efe6fe714d84d7ce62146f18811475163
Dongcp <d-chu.run@gmofh.com>
2019-08-02 12:03:50 +0900

    init

---
+++ BaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseApp
+//
+//  Created by チュ フォン ドン on 8/1/19.
+//  Copyright © 2019 Click. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

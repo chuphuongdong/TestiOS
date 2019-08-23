0e60afd2a2f1ee258c49ac971bff8587ab8b5c89
Dongcp <d-chu.run@gmofh.com>
2019-08-23 18:33:35 +0900

    z

--- BaseAppTests/BaseAppTests.swift
+++ BaseAppTests/BaseAppTests.swift
@@ -59,7 +59,7 @@ class BaseAppTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual("a", "a")
-        XCTAssertEqual("a", "c")
+        XCTAssertEqual("a", "a")
     }
 
     func testPerformanceExample() {
